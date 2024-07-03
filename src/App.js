6549d006dadf407f7c8573642e6b302ebaaa169f
ShahareyarKhan <shahareyar2003@gmail.com>
2024-07-03 19:57:32 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Home from './Components/Home';
-import About from './Components/About';
-import Header from './Components/Header';
-import Service from './Components/Service';
 
 const App = () => {
-    return (
-        <div>
-            <Router>
-                <Header/>
-                <Routes>
-                    <Route path='/'  element={<Home />}/>
-                    <Route path='/about'  element={<About/>}/>
-                    <Route path='/service'  element={<Service/>}/>
-                </Routes>
-            </Router>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className='text-2xl text-center my-9 font-bold'>Props Tutorial</h1>
+    </div>
+  )
 }
 
 export default App
